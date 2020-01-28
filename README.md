1aa180aeca280295943ca67c0d383cd744ca3a36
Ronnan30 <59918074+Ronnan30@users.noreply.github.com>
2020-01-28 18:55:36 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Algorithim
+# bounceOffAlgorithm
+Algorithm to bounce off two objects
---
+++ sketch.js
@@ -0,0 +1,36 @@
+var fixedRect, movingRect;
+var rect1, rect2;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400, 800,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+
+  movingRect.velocityY = -5;
+  fixedRect.velocityY = +5;
+
+  rect1=createSprite(800,100,50,80)
+  rect2=createSprite(300,100,50,80)
+
+  rect1.velocityX= -5
+  rect2.velocityX=  5
+
+
+}
+
+function draw() {
+  background(0,0,0);  
+bounceoff (rect1,rect2) 
+
+
+  drawSprites();
+}
+  
+
+
+
+
